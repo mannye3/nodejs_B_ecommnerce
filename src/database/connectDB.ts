fc21050c0e28d4fa4639b75a95e0db7c6d4c103f
ea <aboajahemmanuel@gmail.com>
2024-08-09 11:56:04 +0100

    Ecommerce API

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const connectDb = async () => {
     try {
         const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-    // const connect = await mongoose.connect("mongodb+srv://manny_e3:goodboy4u.com@cluster0.ik9ayjs.mongodb.net/ecommerce?retryWrites=true&w=majority")
+   
 
         console.log("Database connected: ", connect.connection.host,connect.connection.name)
     } catch (err) {
